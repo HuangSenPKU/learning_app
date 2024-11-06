cf2ae3e30a26ab4299c71433fa1e0597c35a643a
HuangSen <96167958+HuangSenPKU@users.noreply.github.com>
2024-11-06 13:46:22 +0800

    add IPC

--- render.js
+++ render.js
@@ -1,3 +1,10 @@
 const information = document.getElementById('info')
 information.innerHTML = `This app is using chrome version: ${window.versions.chrome()}, \
     Node.js (v${versions.node()}), and electron version: ${window.versions.electron()}`
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // prints out 'pong'
+    }
+    
+func()
